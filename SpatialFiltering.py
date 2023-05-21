c2a5a8c87c767c92881255751ccc88f86bf1d873
Bedirhan Elçin <79866900+BedirhanE@users.noreply.github.com>
2023-05-22 01:20:43 +0300

    çıktı da oluşan görüntüyü kaydetme işlemi eklendi

--- SpatialFiltering.py
+++ SpatialFiltering.py
@@ -36,7 +36,7 @@ ax[1].imshow(filtered_img, cmap='gray')
 ax[1].set_title('Spatial Linear Filtered Image')
 plt.show()
 
-# Çıktı görüntüsü için dosya adını belirtip kaydetme işlemi
+# Çıktı görüntüsü için dosya adını belirtip kaydetme işlemi.
 output_filename = "filtered_image.jpg"
 cv2.imwrite(output_filename, filtered_img.astype(np.uint8))
 print("Filtered image saved as", output_filename)
